64a48ef09f6ac80e034996efd0b3786fd073774b
Guillaume Aquilina <guillaume@workflowai.com>
2025-08-14 15:53:23 +0200

    fix: remove source

--- docs/source.config.ts
+++ docs/source.config.ts
@@ -1,10 +1,11 @@
+import { rehypeCode } from "fumadocs-core/mdx-plugins";
 import {
   defineConfig,
   defineDocs,
   frontmatterSchema,
   metaSchema,
-} from 'fumadocs-mdx/config';
-import { rehypeCode } from 'fumadocs-core/mdx-plugins';
+} from "fumadocs-mdx/config";
+import { remarkTemplateReplacement } from "./lib/template-replacement";
 
 // You can customise Zod schemas for frontmatter and `meta.json` here
 // see https://fumadocs.vercel.app/docs/mdx/collections#define-docs
@@ -21,5 +22,7 @@ export default defineConfig({
   mdxOptions: {
     // MDX options
     rehypePlugins: [rehypeCode],
+    // run remarkTemplateReplacement first
+    remarkPlugins: (v) => [remarkTemplateReplacement, ...v],
   },
 });
