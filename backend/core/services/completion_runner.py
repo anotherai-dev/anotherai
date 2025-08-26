8bd29558bc678c7b48166a68889f93cd3730c21d
Guillaume Aquilina <guillaume@workflowai.com>
2025-08-26 12:30:02 +0200

    feat: connect cache to completion runner

--- backend/core/services/completion_runner.py
+++ backend/core/services/completion_runner.py
@@ -1,8 +1,10 @@
+import asyncio
 from typing import Any
 
 import structlog
 
 from core.domain.agent import Agent
+from core.domain.agent_completion import AgentCompletion
 from core.domain.agent_input import AgentInput
 from core.domain.cache_usage import CacheUsage
 from core.domain.fallback_option import FallbackOption
@@ -15,6 +17,8 @@ from core.storage.agent_storage import AgentStorage
 from core.storage.completion_storage import CompletionStorage
 from core.storage.file_storage import FileStorage
 from core.utils.background import add_background_task
+from core.utils.coroutines import capture_errors
+from core.utils.uuid import uuid7
 
 log = structlog.get_logger(__name__)
 
@@ -36,6 +40,27 @@ class CompletionRunner:
         self._provider_factory = provider_factory
         self._completion_storer = CompletionStorer(completion_storage, agent_storage, file_storage)
 
+    async def _from_cache(
+        self,
+        completion_id: str,
+        agent: Agent,
+        version: Version,
+        input: AgentInput,
+    ) -> AgentCompletion | None:
+        async with asyncio.timeout(0.150):  # 150 ms, cached output clickhouse query should exit after 100ms
+            agent_output = await self._completion_storage.cached_output(version.id, input.id)
+            if not agent_output:
+                return None
+            return AgentCompletion(
+                id=completion_id,
+                agent=agent,
+                version=version,
+                agent_input=input,
+                agent_output=agent_output,
+                messages=[],
+                traces=[],
+            )
+
     async def run(
         self,
         agent: Agent,
@@ -49,6 +74,15 @@ class CompletionRunner:
         completion_id: str | None,
         conversation_id: str | None,
     ):
+        completion_id = completion_id or str(uuid7())
+
+        # Attempt to retrieve from cache if possible
+        if use_cache == "always" or (use_cache == "auto" and version.should_use_auto_cache()):
+            with capture_errors(log, "Error fetching cached output"):
+                completion = await self._from_cache(completion_id, agent, version, input)
+                if completion:
+                    return completion
+
         # TODO: cache
         runner = Runner(
             tenant_slug=self._tenant.slug,
