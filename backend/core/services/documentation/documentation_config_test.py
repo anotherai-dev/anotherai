f9a26556b2847661e034806aa080556e6d5ca011
Guillaume Aquilina <guillaume@workflowai.com>
2025-09-09 09:42:29 +0200

    test: fix test_default_docs_directory

--- backend/core/services/documentation/documentation_config_test.py
+++ backend/core/services/documentation/documentation_config_test.py
@@ -6,6 +6,6 @@ from core.services.documentation.documentation_config import _default_docs_direc
 
 def test_default_docs_directory():
     path, config = _default_docs_directory()
-    assert path.endswith("docs")
+    assert str(path).endswith("docs")
     assert config.environments
     assert config.default
